8d7e4722cbce9c7ab990a6c8cafda2632b031d31
Idan <newuser@gmail.com>
2018-10-01 11:21:20 +0300

    Add endpoints for creation of rental and search rental by city

--- src/app/server/fake-db.js
+++ src/app/server/fake-db.js
@@ -2,167 +2,175 @@ const Rental = require('./models/rental');
 
 const User = require ('./models/user');
 
+const Booking = require ( './models/booking' );
+
+const fakeDbData = require ( './data.json' );
+
 class FakeDb {
 
     constructor() {
 
-        this.rentals = [{
+        this.rentals = fakeDbData.rentals;
+        
+//         [{
 
-    category: 'Small Car',
-    model: 'Suzuki Alto',
-    company: 'Suzuki',    
-    year: 2014,
-    seats: 4,
-    doors: 4,
-    specificiations: 'Manual gearbox',
-    dailyRate: 46,
-    image: 'https://cdn2.rcstatic.com/images/car_images/new_images/suzuki/alto_lrg.jpg',
-    pickup: 'Ben Gurion Airport, terminal 3, Tel Aviv-Yafo',
-    dropoff: 'Sde Dov Airport, Tel Aviv-Yafo'
-  },
+//     category: 'Small Car',
+//     model: 'Suzuki Alto',
+//     company: 'Suzuki',    
+//     year: 2014,
+//     seats: 4,
+//     doors: 4,
+//     specificiations: 'Manual gearbox',
+//     dailyRate: 46,
+//     image: 'https://cdn2.rcstatic.com/images/car_images/new_images/suzuki/alto_lrg.jpg',
+//     pickup: 'Ben Gurion Airport, terminal 3, Tel Aviv-Yafo',
+//     dropoff: 'Sde Dov Airport, Tel Aviv-Yafo'
+//   },
 
-  {
-  category: 'Small Car',
-  model: 'Kia Picanto',
-  company: 'Kia',    
-  year: 2015,
-  seats: 4,
-  doors: 4,
-  specificiations: 'Automatic gearbox',
-  dailyRate: 95,
-  image: 'https://cdn2.rcstatic.com/images/car_images/new_images/kia/picanto_5_door_lrg.jpg',
-  pickup: 'Sde Dov Airport, Tel Aviv-Yafo' ,
-  dropoff:'Ben Gurion Airport, terminal 3, Tel Aviv-Yafo'
-},
+//   {
+//   category: 'Small Car',
+//   model: 'Kia Picanto',
+//   company: 'Kia',    
+//   year: 2015,
+//   seats: 4,
+//   doors: 4,
+//   specificiations: 'Automatic gearbox',
+//   dailyRate: 95,
+//   image: 'https://cdn2.rcstatic.com/images/car_images/new_images/kia/picanto_5_door_lrg.jpg',
+//   pickup: 'Sde Dov Airport, Tel Aviv-Yafo' ,
+//   dropoff:'Ben Gurion Airport, terminal 3, Tel Aviv-Yafo'
+// },
 
-{
-    category: 'Medium Car',
-    model: 'Hyundai i25',
-    company: 'Hyundai',
-    year: 2017,
-    seats: 5,
-    doors: 4,
-    specificiations: 'Automatic gearbox',
-    dailyRate: 54,
-    image: 'https://cdn2.rcstatic.com/images/car_images/new_images/hyundai/i25_accent_lrg.jpg',
-    pickup: 'Ben Gurion Airport, terminal 3, Tel Aviv-Yafo',
-    dropoff: 'Sde Dov Airport, Tel Aviv-Yafo'
-  },
+// {
+//     category: 'Medium Car',
+//     model: 'Hyundai i25',
+//     company: 'Hyundai',
+//     year: 2017,
+//     seats: 5,
+//     doors: 4,
+//     specificiations: 'Automatic gearbox',
+//     dailyRate: 54,
+//     image: 'https://cdn2.rcstatic.com/images/car_images/new_images/hyundai/i25_accent_lrg.jpg',
+//     pickup: 'Ben Gurion Airport, terminal 3, Tel Aviv-Yafo',
+//     dropoff: 'Sde Dov Airport, Tel Aviv-Yafo'
+//   },
 
-  {
-    category: 'Medium Car',
-    model: 'Nissan Micra',
-    company: 'Nissan',
-    year: 2017,
-    seats: 5,
-    doors: 4,
-    specificiations: 'Manual gearbox',
-    dailyRate: 94,
-    image: 'https://cdn2.rcstatic.com/images/car_images/new_images/nissan/micra_lrg.jpg',
-    pickup: 'Sde Dov Airport, Tel Aviv-Yafo',
-    dropoff: 'Ben Gurion Airport, terminal 3, Tel Aviv-Yafo'
-  },
+//   {
+//     category: 'Medium Car',
+//     model: 'Nissan Micra',
+//     company: 'Nissan',
+//     year: 2017,
+//     seats: 5,
+//     doors: 4,
+//     specificiations: 'Manual gearbox',
+//     dailyRate: 94,
+//     image: 'https://cdn2.rcstatic.com/images/car_images/new_images/nissan/micra_lrg.jpg',
+//     pickup: 'Sde Dov Airport, Tel Aviv-Yafo',
+//     dropoff: 'Ben Gurion Airport, terminal 3, Tel Aviv-Yafo'
+//   },
 
 
-  {
-    category: 'Large Car',
-    model: 'Mazda 6',
-    company: 'Mazda',
-    year: 2015,
-    seats: 5,
-    doors: 4,
-    specificiations: 'Automatic gearbox',
-    dailyRate: 78,
-    image: 'https://cdn2.rcstatic.com/images/car_images/new_images/mazda/6_lrg.jpg',
-    pickup: 'Ben Gurion Airport, terminal 3, Tel Aviv-Yafo',
-    dropoff: 'Sde Dov Airport, Tel Aviv-Yafo'
-  },
+//   {
+//     category: 'Large Car',
+//     model: 'Mazda 6',
+//     company: 'Mazda',
+//     year: 2015,
+//     seats: 5,
+//     doors: 4,
+//     specificiations: 'Automatic gearbox',
+//     dailyRate: 78,
+//     image: 'https://cdn2.rcstatic.com/images/car_images/new_images/mazda/6_lrg.jpg',
+//     pickup: 'Ben Gurion Airport, terminal 3, Tel Aviv-Yafo',
+//     dropoff: 'Sde Dov Airport, Tel Aviv-Yafo'
+//   },
 
-  {
-    category: 'Large Car',
-    model: 'Subaru Impreza',
-    company: 'Subaru',
-    year: 2016,
-    seats: 5,
-    doors: 4,
-    specificiations: 'Automatic gearbox',
-    dailyRate: 123,
-    image: 'https://cdn2.rcstatic.com/images/car_images/new_images/subaru/impreza_lrg.jpg',
-    pickup: 'Sde Dov Airport, Tel Aviv-Yafo' ,
-    dropoff: 'Ben Gurion Airport, terminal 3, Tel Aviv-Yafo'  
-  },
+//   {
+//     category: 'Large Car',
+//     model: 'Subaru Impreza',
+//     company: 'Subaru',
+//     year: 2016,
+//     seats: 5,
+//     doors: 4,
+//     specificiations: 'Automatic gearbox',
+//     dailyRate: 123,
+//     image: 'https://cdn2.rcstatic.com/images/car_images/new_images/subaru/impreza_lrg.jpg',
+//     pickup: 'Sde Dov Airport, Tel Aviv-Yafo' ,
+//     dropoff: 'Ben Gurion Airport, terminal 3, Tel Aviv-Yafo'  
+//   },
 
-  {
-  category: 'People Carriers',
-  model: 'Mazda 5',
-  company: 'Mazda',
-  year: 2014,
-  seats: 7,
-  doors: 4,
-  specificiations: 'Automatic gearbox',
-  dailyRate: 231,
-  image: 'https://cdn2.rcstatic.com/images/car_images/new_images/mazda/5_lrg.jpg',
-  pickup: 'Ben Gurion Airport, terminal 3, Tel Aviv-Yafo',
-  dropoff: 'Sde Dov Airport, Tel Aviv-Yafo'
-  },
+//   {
+//   category: 'People Carriers',
+//   model: 'Mazda 5',
+//   company: 'Mazda',
+//   year: 2014,
+//   seats: 7,
+//   doors: 4,
+//   specificiations: 'Automatic gearbox',
+//   dailyRate: 231,
+//   image: 'https://cdn2.rcstatic.com/images/car_images/new_images/mazda/5_lrg.jpg',
+//   pickup: 'Ben Gurion Airport, terminal 3, Tel Aviv-Yafo',
+//   dropoff: 'Sde Dov Airport, Tel Aviv-Yafo'
+//   },
 
-  {
-  category: 'People Carriers',
-  model: 'Citroen Berlingo',
-  company: 'Citroen',
-  year: 2012,
-  seats: 7,
-  doors: 4,
-  specificiations: 'Manual gearbox',
-  dailyRate: 205,
-  image: 'https://cdn2.rcstatic.com/images/car_images/new_images/citroen/berlingo_lrg.jpg',
-  pickup: 'Sde Dov Airport, Tel Aviv-Yafo',
-  dropoff: 'Ben Gurion Airport, terminal 3, Tel Aviv-Yafo' 
-  },
+//   {
+//   category: 'People Carriers',
+//   model: 'Citroen Berlingo',
+//   company: 'Citroen',
+//   year: 2012,
+//   seats: 7,
+//   doors: 4,
+//   specificiations: 'Manual gearbox',
+//   dailyRate: 205,
+//   image: 'https://cdn2.rcstatic.com/images/car_images/new_images/citroen/berlingo_lrg.jpg',
+//   pickup: 'Sde Dov Airport, Tel Aviv-Yafo',
+//   dropoff: 'Ben Gurion Airport, terminal 3, Tel Aviv-Yafo' 
+//   },
 
-    {
-  category: 'SUV (sport utility vehicle)',
-  model: 'Opel Mokka',
-  company: 'Opel',
-  year: 2013,
-  seats: 5,
-  doors: 4,
-  specificiations: 'Automatic gearbox',
-  dailyRate: 192,
-  image: 'https://cdn2.rcstatic.com/images/car_images/new_images/opel/mokka_lrg.jpg',
-  pickup: 'Ben Gurion Airport, terminal 3, Tel Aviv-Yafo',
-  dropoff: 'Sde Dov Airport, Tel Aviv-Yafo'
-      },
+//     {
+//   category: 'SUV (sport utility vehicle)',
+//   model: 'Opel Mokka',
+//   company: 'Opel',
+//   year: 2013,
+//   seats: 5,
+//   doors: 4,
+//   specificiations: 'Automatic gearbox',
+//   dailyRate: 192,
+//   image: 'https://cdn2.rcstatic.com/images/car_images/new_images/opel/mokka_lrg.jpg',
+//   pickup: 'Ben Gurion Airport, terminal 3, Tel Aviv-Yafo',
+//   dropoff: 'Sde Dov Airport, Tel Aviv-Yafo'
+//       },
 
-    {
-  category: 'SUV (sport utility vehicle)',
-  model: 'Mitsubishi Outlander',
-  company: 'Mitsubishi',
-  year: 2015,
-  seats: 7,
-  doors: 4,
-  specificiations: 'Automatic gearbox',
-  dailyRate: 157,
-  image: 'https://cdn2.rcstatic.com/images/car_images/new_images/mitsubishi/outlander_lrg.jpg',
-  pickup: 'Sde Dov Airport, Tel Aviv-Yafo',
-  dropoff: 'Ben Gurion Airport, terminal 3, Tel Aviv-Yafo'
-    }];
+//     {
+//   category: 'SUV (sport utility vehicle)',
+//   model: 'Mitsubishi Outlander',
+//   company: 'Mitsubishi',
+//   year: 2015,
+//   seats: 7,
+//   doors: 4,
+//   specificiations: 'Automatic gearbox',
+//   dailyRate: 157,
+//   image: 'https://cdn2.rcstatic.com/images/car_images/new_images/mitsubishi/outlander_lrg.jpg',
+//   pickup: 'Sde Dov Airport, Tel Aviv-Yafo',
+//   dropoff: 'Ben Gurion Airport, terminal 3, Tel Aviv-Yafo'
+//     }];
 
-    this.users = [{
+     this.users = fakeDbData.users;
+     
+    //  [{
 
-      username: "Test User",
+    //   username: "Test User",
 
-      email: "test@gmail.com",
+    //   email: "test@gmail.com",
 
-      password: "testtest"
-    }, {
+    //   password: "testtest"
+    // }, {
 
-      username: "Test User1",
+    //   username: "Test User1",
 
-      email: "test1@gmail.com",
+    //   email: "test1@gmail.com",
 
-      password: "testtest1"
-    }];
+    //   password: "testtest1"
+    // }];
   }
 
 async cleanDb() {
@@ -170,6 +178,8 @@ async cleanDb() {
     await User.remove({});
 
     await Rental.remove({});
+
+    await Booking.remove({});
 }
 
 pushDataToDb() {
--- src/app/server/routes/rentals.js
+++ src/app/server/routes/rentals.js
@@ -2,27 +2,17 @@ const express = require ('express');
 
 const router = express.Router();
 
+const User = require('../models/user');
+
 const Rental = require('../models/rental');
+
+const { normalizeErrors } = require ( '../helpers/mongoose' );
 
 const UserCtrl = require ('../controllers/user');
 
 router.get ('/secret', UserCtrl.authMiddleware, function ( req, res ) {
 
     res.json ({ "secret": true });
-});
-
-router.get ('', function ( req, res ) {
-
-    Rental.find ({})
-
-          .select ( '-bookings' )
-
-          .exec ( function ( err, foundRentals ) { 
-
-        res.json (foundRentals);
-    })
-
-    /* res.json ({'ok': true}); */
 });
 
 router.get('/:id', function (req, res){
@@ -45,6 +35,82 @@ Rental.findById ( rentalId )
          return res.json (foundRental);
       })
     });
-    
+
+router.post ( '', UserCtrl.authMiddleware, function ( req, res ){
+
+    const { category, model, company, year, seats, doors, specificiations, dailyRate, image, pickup, dropoff } = req.body;
+
+    const user = res.locals.user;
+
+    const rental = new Rental ( { category, model, company, year, seats, doors, specificiations, dailyRate, image, pickup, dropoff });
+
+    rental.user = user;
+
+    Rental.create ( rental, function ( err, newRental){
+
+        if ( err ) {
+
+            return res.status ( 422 ).send ({ errors: normalizeErrors ( err.errors )});
+        }
+
+        User.update ({ _id: user.id }, { $push: { rentals: newRental }}, function () {});
+
+        return res.json ( newRental );
+    })
+
+})
+
+router.get ('', function ( req, res ) {
+
+    const category = req.query.category;
+
+    const query = category ? { category: category.toLowerCase () } : {};
+
+    // if ( category ) {
+
+    //     Rental.find ({ category: category.toLowerCase () })
+
+    //           .select ( '-bookings' )
+
+    //           .exec ( function ( err, filteredRentals ) { 
+
+    // if ( err ) {
+
+    //     return res.status ( 422 ).send ({ errors: normalizeErrors ( err.errors )});
+    // }
+
+    // if ( filteredRentals.length === 0 ) {
+
+    //       return res.status(422).send ({ errors: [{ title: 'No Rental Found', detail: `There are no rentals for category ${ category }`}]});
+    // }
+
+    // return res.json ( filteredRentals );
+
+    //           })
+    // } else {
+
+    Rental.find ( query )
+
+          .select ( '-bookings' )
+
+          .exec ( function ( err, foundRentals ) { 
+
+    if ( err ) {
+
+          return res.status ( 422 ).send ({ errors: normalizeErrors ( err.errors )});
+
+               }
+        
+    if ( category && foundRentals.length === 0 ) {
+        
+          return res.status(422).send ({ errors: [{ title: 'No Rentals Found', detail: `There are no rentals for category ${ category }`}]});
+
+        }                               
+
+        return res.json ( foundRentals );
+    })
+}
+    /* res.json ({'ok': true}); */
+);    
 
 module.exports = router;
